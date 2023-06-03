24f1cf1553141d3f475445b165d34f23ec1bff63
spahiarl <71666567+spahiarl@users.noreply.github.com>
2023-06-03 15:35:13 +0200

    Testergebnis alle (1 Fail)

--- backend/src/test/java/ch/zhaw/iwi/devops/stringcalculator/StringCalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/stringcalculator/StringCalculatorTest.java
@@ -23,5 +23,38 @@ public class StringCalculatorTest {
         int result = calculator.add("2,3");
         Assertions.assertEquals(5, result);
     }
-    
+    @Test
+    public void testMehrereZahlen() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1,2,3,4,5");
+        Assertions.assertEquals(15, result);
+    }
+    @Test
+    public void testZahlenMitUmbruch() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("1\n2,3");
+        Assertions.assertEquals(6, result);
+    }
+    @Test
+    public void testZahlenMitSpezTrennZ() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("//;\n1;2;3");
+        Assertions.assertEquals(6, result);
+    }
+    @Test
+    public void testNegativeZahlen() {
+        StringCalculator calculator = new StringCalculator();
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            calculator.add("1,-2,3,-4");
+        });
+        String expectedMessage = "Negatives not allowed: -2 -4";
+        String actualMessage = exception.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+    @Test
+    public void testZahlenGrösserAls1000Ignorieren() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("2,1001,6");
+        Assertions.assertEquals(8, result);
+    }
 }
