e391d357391f59b170ee24b26d4cdaf08d3e3a56
spahiarl <71666567+spahiarl@users.noreply.github.com>
2023-06-03 15:31:43 +0200

    Testergebnis 3

--- backend/src/test/java/ch/zhaw/iwi/devops/stringcalculator/StringCalculatorTest.java
+++ backend/src/test/java/ch/zhaw/iwi/devops/stringcalculator/StringCalculatorTest.java
@@ -17,5 +17,11 @@ public class StringCalculatorTest {
         int result = calculator.add("5");
         Assertions.assertEquals(5, result);
     }
+    @Test
+    public void testZweiZahlen() {
+        StringCalculator calculator = new StringCalculator();
+        int result = calculator.add("2,3");
+        Assertions.assertEquals(5, result);
+    }
     
 }
